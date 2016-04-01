cc06bb31b8dc7b412dfffc14daf76f8669e5b664
ZhongliangYuan <5681713@qq.com>
2016-04-01 08:32:55 +0800

    remove idea and change to new nap_api

--- rest_api/views.py
+++ rest_api/views.py
@@ -4,13 +4,12 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import authenticate
 import ast
 
 from rest_framework.decorators import (
-        api_view, 
-        authentication_classes, 
-        permission_classes
+    api_view,
+    authentication_classes,
+    permission_classes
 )
 
 # from compose import app_info, project_create
@@ -24,7 +23,7 @@ def project_list(request, format=None):
     """
     list all servie or create a service
     """
-    username,passwd = str(request.user), str(request.user)
+    username = str(request.user)
 
     if request.method == 'GET':
         ret_data = {}
@@ -33,9 +32,9 @@ def project_list(request, format=None):
             begin_index = int(request.GET['start'])
             length = int(request.GET['limit'])
         except:
-            return Response({},status=status.HTTP_400_BAD_REQUEST)
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
-        poj_list = app_info.project_list(username, passwd, begin_index, length)
+        poj_list = app_info.project_list(username, begin_index, length)
 
         # get all service name
         ret_data['success'] = "true"
@@ -57,7 +56,7 @@ def project_list(request, format=None):
             except:
                 return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
-            sts, msg = project_create.create_project_from_url(username, passwd, projname, projurl)
+            sts, msg = project_create.create_project_from_url(username, projname, projurl)
             if sts == 'Argv':
                 return Response({'paras': 'true', 'argv': msg})
             else:
@@ -65,7 +64,7 @@ def project_list(request, format=None):
 
         # create proejct from filebrowser
         elif cmd == 'file':
-            sts, msg = project_create.create_project_from_file(username, passwd, projname)
+            sts, msg = project_create.create_project_from_file(username, projname)
             if sts == 'Argv':
                 return Response({'paras': 'true', 'argv': msg})
             else:
@@ -78,29 +77,26 @@ def project_list(request, format=None):
             except:
                 return Response({}, status=status.HTTP_400_BAD_REQUEST)
             argv_dict = ast.literal_eval(argv)
-            sts, msg = project_create.replace_argv(username, passwd, projname, argv_dict)
+            sts, msg = project_create.replace_argv(username, projname, argv_dict)
             return Response({'log': msg})
 
 
 @api_view(['DELETE', 'GET'])
 @authentication_classes((TokenAuthentication,))
 @permission_classes((IsAuthenticated,))
-def project(request, project, format=None):
+def project(request, pro, format=None):
     """
     delete a project, or get services from a project
     """
-    username, passwd = str(request.user), str(request.user)
+    username = str(request.user)
 
     if request.method == 'DELETE':
-        sts, logs = app_info.destroy_project(username, passwd, project)
+        sts, logs = app_info.destroy_project(username, pro)
         return Response({'Delete': sts, 'log': logs})
 
     elif request.method == 'GET':
-        services = app_info.service_list(username, passwd, project)
-        ret_data = {}
-        ret_data['success'] = 'true'
-        ret_data['total'] = len(services)
-        ret_data['services'] = services
+        services = app_info.service_list(username, pro)
+        ret_data = {'success': 'true', 'total': len(services), 'services': services}
         return Response(ret_data)
 
 
@@ -112,14 +108,14 @@ def service_list(request, format=None):
     list services of a project
     """
     ret_data = {}
-    username,passwd = str(request.user), str(request.user)
+    username = str(request.user)
 
     try:
         project_name = request.GET['project']
     except:
         return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
-    services = app_info.service_list(username, passwd, project_name)
+    services = app_info.service_list(username, project_name)
 
     ret_data['success'] = 'true'
     ret_data['total'] = len(services)
@@ -135,19 +131,19 @@ def log(request, format=None):
     get logs of a specific service
     """
     ret_data = {}
-    username,passwd = str(request.user), str(request.user)
+    username = str(request.user)
 
     try:
         project_name = request.GET['project']
         service_name = request.GET['service']
     except:
         print "here"
-        return Response({}, status = status.HTTP_400_BAD_REQUEST)
-    
-    logs = app_info.get_logs(username, passwd, project_name, service_name)
+        return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+    logs = app_info.get_logs(username, project_name, service_name)
     ret_data['success'] = 'true'
     ret_data['logs'] = logs
-    
+
     return Response(ret_data)
 
 
